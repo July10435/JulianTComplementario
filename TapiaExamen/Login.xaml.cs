21a8cc25d9356095f9002132b5a87d8bf3d3a69f
Julian Tapia <Julian Tapia@JULY10435>
2021-10-29 21:11:00 -0500

    Agregar archivos de proyecto.

---
+++ TapiaExamen/Login.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TapiaExamen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btn_ingresar_Clicked(object sender, EventArgs e)
+        {
+            String usuario = Convert.ToString(txt_usuario.Text);
+            String contrasena = Convert.ToString(txt_contrasena.Text);
+
+            if (usuario == "estudiante2021" && contrasena == "uisrael2021")
+            {
+                await Navigation.PushAsync(new Registro());
+            }
+            else
+                await DisplayAlert("Ingreso", "Fallido", "OK");
+        }
+    }
+}
---
+++ TapiaExamen/Registro.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TapiaExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        String nombre = null;
+        Double pagoIni = 0;
+        Double pagofinal = 0;
+
+        public Registro()
+        {
+
+            InitializeComponent();
+        }
+
+        private void btn_calcular_Clicked(object sender, EventArgs e)
+        {
+
+            pagoIni = Convert.ToDouble(txt_montoini.Text);
+
+
+            Double cuotas;
+            Double interes = 0;
+
+            if (pagoIni < 1800)
+            {
+                cuotas = (1800 - pagoIni) / 3;
+                interes = (1800 * 0.05) + cuotas;         
+                txt_pagomen.Text = Convert.ToString(interes);
+            }
+
+        }
+
+        private async void btn_registrar_Clicked(object sender, EventArgs e)
+        {
+            nombre = Convert.ToString(txt_name.Text);
+            await DisplayAlert("Ingreso", "Elemento guardado con exito", "OK");
+
+
+            await Navigation.PushAsync(new Resumen(pagofinal, nombre));
+        }
+    }
+}
